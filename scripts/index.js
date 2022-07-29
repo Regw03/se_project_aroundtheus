3f8e64f794aca245b991156855f494e39a6585c6
Regw03 <regw03@gmail.com>
2022-07-28 19:26:13 -0600

    proj5 2nd

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-// cards array
+/* -------------------------------------------------------------------------- */
+/*                               // cards array                               */
+/* -------------------------------------------------------------------------- */
 
 const initialCards = [
 { 
@@ -29,44 +31,77 @@ const initialCards = [
 
 
 
+/* -------------------------------------------------------------------------- */
+/*                               //buttons nodes                              */
+/* -------------------------------------------------------------------------- */
 
-
-
-//buttons nodes
+//like button
+const likeButton = document.querySelector(".elements__card-button");
+//add button
 const addCard = document.querySelector(".profile__add-button");
+//trash can
 const cardDelete = document.querySelector(".elements__trash-button");
+//edit button
 const profileEditButton = document.querySelector(".profile__edit-button");
+//popup close button
 const profileEditClose = document.querySelector(".popup__close-button");
+//save button
 const profileSave = document.querySelector(".popup__submit");
-//wrappers
+//like button active
+//const likebuttonActive = document.querySelector(".elements__card-button:active");
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 //wrappers                                 */
+/* -------------------------------------------------------------------------- */
+
 const profileTitle = document.querySelector(".profile__info-name");
 const profileProfession = document.querySelector(".profile__info-profession");
 const popupEditForm = document.querySelector(".popup__form");
+//edit popup
 const profileEditPopup = document.querySelector("#edit-popup");
+//add popup
 const addCardPopup = document.querySelector("#add-popup");
-// form data
+
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                // form data                                */
+/* -------------------------------------------------------------------------- */
+//edit form data
 const nameInputValue = popupEditForm.querySelector(".popup__input-name");
 const professionInputValue = popupEditForm.querySelector(".popup__input-profession");
 
-// card template
+//card add form data
+
+
+/* -------------------------------------------------------------------------- */
+/*                              // card template                              */
+/* -------------------------------------------------------------------------- */
 
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 const cardListEl = document.querySelector(".elements__card-grid");
 
 
-//  profile edit form
+/* -------------------------------------------------------------------------- */
+/*                            //  profile edit form                           */
+/* -------------------------------------------------------------------------- */
  
+//edit popup is open event
 profileEditButton.addEventListener("click", function() {
     nameInputValue.value = profileTitle.textContent;
     professionInputValue.value = profileProfession.textContent;
     profileEditPopup.classList.add("popup__is_open");
 });
 
+//edit popup close event
 profileEditClose.addEventListener("click", function(){
     profileEditPopup.classList.remove("popup__is_open");
 });
 
-
+//profile save event
 popupEditForm.addEventListener("submit", (event) =>{
     event.preventDefault();
     const nameInputValue = event.target.name.value; 
@@ -78,30 +113,30 @@ popupEditForm.addEventListener("submit", (event) =>{
 });
 
 
-//cards add form
 
-addCard.addEventListener("click", function(){
-    nameInputValue.value = profileTitle.textContent;
-    professionInputValue.value = profileProfession.textContent;
-    profileEditPopup.classList.add("popup__is_open");
-})
-
-
-// cards ellement 
+/* -------------------------------------------------------------------------- */
+/*                              // cards ellement                             */
+/* -------------------------------------------------------------------------- */
 
 function createCard (cardData){
   // clone template
   const cardElement = cardTemplate.cloneNode(true);
+
   // find card image
   const imageEl = cardElement.querySelector(".elements__card-image");
+
   // find card title
    const titleEl = cardElement.querySelector(".elements__card-title");
+
   // replace img src
   imageEl.src = cardData.link;
+
   // replace img alt
   imageEl.alt = cardData.name;
+
   // replace title
   titleEl.textContent = cardData.name;
+
   // append to list
   return cardElement;
 };
@@ -109,8 +144,21 @@ function createCard (cardData){
 function renderCard (cardData){
   const cardElement = createCard(cardData);
   cardListEl.prepend(cardElement);
-}
+};
 
 initialCards.forEach(renderCard);
 
 
+//cards add form
+
+addCard.addEventListener("click", function(){
+    nameInputValue.value = profileTitle.textContent;
+    professionInputValue.value = profileProfession.textContent;
+    profileEditPopup.classList.add("popup__is_open");
+});
+
+//cards like event
+
+
+
+//cards delete event
