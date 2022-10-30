e165c0e5e7d572de5c9777d4461b3ffd2b43e55d
Regw03 <regw03@gmail.com>
2022-10-30 07:40:26 -0600

    proj7 2nd commit

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,40 +1,46 @@
 class FormValidator {
 
     constructor(settings, formElement) {
+        this._settings = settings;
+        this._form = formElement;
         this._formSelector = settings.formSelector;
         this._inputSelector = settings.inputSelector;
         this._submitButtonSelector = settings.submitButtonSelector;
         this._inactiveButtonClass = settings.inactiveButtonClass;
         this._inputErrorClass = settings.inputErrorClass;
         this._errorClass = settings.errorClass;
-
+        this.settings = settings;
         this._form = formElement;
-    };
-
-    _setEventListeners() {
-
-        const inputList = [...this._form.querySelectorAll(settings.inputSelector)];
-        const submitButton = this._form.querySelector(settings.submitButtonSelector);
-        toggleButton(inputList, submitButton, settings);
-
-        inputList.forEach((input) => {
-            input.addEventListener("input", (e) => {
-                //check validation
-                checkInputValidity(this._form, input, settings);
-                //toggle the button
-                toggleButton(inputList, submitButton, settings);
-            
-             });
-        });
-
-    }
-
-    enableValidation() {
-
-        this._form.addEventListener("submit", (e) => e.preventDefault())
-        setEventListeners(this._form, settings);
         
+        console.log(this);
     };
+        
+
+
+    // _setEventListeners() {
+
+    //     const inputList = [...this._form.querySelectorAll(this.settings.inputSelector)];
+    //     const submitButton = this._form.querySelector(this.settings.submitButtonSelector);
+    //     toggleButton(inputList, submitButton, settings);
+
+    //     inputList.forEach((input) => {
+    //         input.addEventListener("input", (e) => {
+    //             //check validation
+    //             checkInputValidity(this._form, input, settings);
+    //             //toggle the button
+    //             toggleButton(inputList, submitButton, settings);
+            
+    //          });
+    //     });
+
+    // }
+
+    // _enableValidation() {
+
+    //     this._form.addEventListener("submit", (e) => e.preventDefault())
+    //     setEventListeners(this._form, settings);
+        
+    // };
 
 };
 
@@ -42,4 +48,4 @@ class FormValidator {
 const editFormValidator = new FormValidator(settings, editForm);
 editFormValidator.enableValidation();
 
-const addFormValidator = new FormValidator(settign, addForm);
+const addFormValidator = new FormValidator(settings, addForm);
