7f2a0276fa3c9f27b6d59db7010c49432d03e9cc
Regw03 <regw03@gmail.com>
2023-07-21 21:07:42 -0600

    proj9 2

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,17 +3,36 @@ class Api {
     // constructor body
   }
 
-  getInitialCards() {
-    // ...
-  }
+   getInitialCards() {
+
+   return fetch("https://around-api.en.tripleten-services.com/v1", {
+
+     headers: {
+       authorization: " 4b5610a4-d4fb-4ac3-a0cf-5cf08fc2641b",
+     },
+
+   })
+   
+   .then((res) => {
+
+     if (res.ok) {
+       return res.json();
+     }
+
+   });
+
+ }
+ 
 
   // other methods for working with the API
 }
 
 const api = new Api({
-  baseUrl: "https://around.nomoreparties.co/v1/group-42",
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+
   headers: {
-    authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
+    authorization: "4b5610a4-d4fb-4ac3-a0cf-5cf08fc2641b",
     "Content-Type": "application/json",
   },
+
 });
